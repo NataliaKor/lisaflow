00f0bf0a953c9f292ada0ef1b24dc3656ac8aab3
Natalia Korsakova <natalia.korsakova@obspm.fr>
2023-09-13 15:57:21 +0200

    Initial file

---
+++ flow/experiments/__init__.py
@@ -0,0 +1,11 @@
+from flow.experiments.data_loader import(
+    NPYDataset
+)
+
+from flow.experiments.data_loader_sky import (
+    NPYDataset
+)
+
+from flow.experiments.data_loader_galaxy import (
+    GalaxyDataset
+)
