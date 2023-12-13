386dfc931117902c05309bcdf610d70159350cf5
Natalia Korsakova <natalia.korsakova@obspm.fr>
2023-12-13 17:51:34 +0100

    Added Jacobian of the original transform to logprob

--- flow/experiments/rvs/galaxy_ffdot.py
+++ flow/experiments/rvs/galaxy_ffdot.py
@@ -65,7 +65,7 @@ class GalaxyFFdot(nn.Module):
 
         self.flow = flow
 
-    def sample(self, num_samples):
+    def sample_and_log_prob(self, num_samples):
         """Sample from the extimated distribution.
 
         Args:
@@ -123,16 +123,23 @@ class GalaxyFFdot(nn.Module):
         Returns:
             log probability of the inputs
         """
-        inputs = torch.as_tensor(inputs_cupy, device = self.dev)
+        inputs_nonorm = torch.as_tensor(inputs_cupy, device = self.dev)
         self.flow.eval()
         with torch.no_grad():
-            inputs = 2.0*(inputs - self.param_min)/(self.param_max - self.param_min) - 1.0
+            inputs = 2.0*(inputs_nonorm - self.param_min)/(self.param_max - self.param_min) - 1.0
+            log_prob = self.flow.log_prob(inputs)
 
-            inputs[:,0] = torch.exp(inputs[:,0])
-            inputs[:,1] = torch.sign(inputs[:,1])*torch.pow(10,(samples[:,1]))
+            # Jacobian of the forward transform
+            inputs_nonorm[:,0] = torch.exp(inputs_nonorm[:,0])
+            inputs_nonorm[:,1] = torch.sign(inputs_nonorm[:,1])*torch.pow(10,(inputs_nonorm[:,1]))
+            # I am not sure that everything is correct here with the sign!
+            log_prob_norm1_forward = cp.log(cp.log(10)) - cp.log(inputs_nonorm[:,0]) - cp.sign(inputs_nonorm[:,1])*cp.log(cp.abs(inputs_nonorm[:,1])) 
+            log_prob_norm2_forward = cp.log(8) - cp.log(self.param_max[0] - self.param_min[0]) - \
+                                                 cp.log(self.param_max[1] - self.param_min[1]) - \
+                                                 cp.log(self.param_max[0] - self.param_min[0])
 
             log_prob = self.flow.log_prob(inputs)
-        log_prob_cupy = cp.asarray(log_prob)
+        log_prob_cupy = cp.asarray(log_prob) + log_prob_norm1_forward + log_prob_norm2_forward
         return log_prob_cupy
 
 
