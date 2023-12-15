b6df90f7dccdb2698e603daac914f739da616102
Natalia Korsakova <natalia.korsakova@obspm.fr>
2023-12-15 14:08:54 +0100

    Updates to the galaxy distributions

--- flow/experiments/rvs/galaxy.py
+++ flow/experiments/rvs/galaxy.py
@@ -1,33 +1,23 @@
 # In the future create base class for fiting and sampling distributions.
 # Inherit Galaxy class from the base class.
-import numpy as np
-
 import torch
 import cupy as cp
-
-from flow.utils.torchutils import *
-from flow.distributions.normal import *
-
-from flow.experiments.flow_architecture_density_small import *
-
 from rv_base import RV_base
 
 class Galaxy(RV_base):
     """
-        Multivariate Gaussian distribution with zero mean and unit covariance matrix.
+        Class for the Galaxy distribution.
     """
     def __init__(self, config_file):
         super(Galaxy, self).__init__(config_file)
 
-
     def _log_prob(self, inputs_cupy):
         """Calculate log probability for the sample.
            All parameters unnormalised, normalisation performed inside function.
-           Amplitude in the actual units and not in log.
- 
+           Amplitude is passed to a function in log10.
+           Order of the parameters [log10(A), sin(beta), lambda]. 
         Args:
-            inputs: sample prom the distribution
-            type: cupy tensor
+            inputs_cupy: samples from the distribution
         Returns:
             log probability of the inputs
         """
@@ -38,12 +28,10 @@ class Galaxy(RV_base):
             log_prob = self.flow.log_prob(inputs)
  
             # Jacobian of the forward transform
-            log_prob_norm1_forward = cp.log(cp.log(10)) - cp.log(cp.power(10,inputs_nonorm[:,0]))
+            log_prob_norm1_forward = cp.log(cp.log(10)) - cp.log(cp.power(10,inputs_nonorm[:,0])) + \
+                                                          cp.cos(inputs_nonorm[:,1]) 
             log_prob_norm2_forward = cp.log(8) - cp.log(self.param_max[0] - self.param_min[0]) - \
                                                  cp.log(self.param_max[1] - self.param_min[1]) - \
                                                  cp.log(self.param_max[0] - self.param_min[0])
         log_prob_cupy = cp.asarray(log_prob) + log_prob_norm1_forward + log_prob_norm2_forward
         return log_prob_cupy
-
-
-
