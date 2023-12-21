a19fabf593e51654e38efc72203c255b805881fd
Natalia Korsakova <natalia.korsakova@obspm.fr>
2023-12-21 12:33:26 +0100

    Added plotting to data loaders

--- flow/experiments/data_loader_1gb.py
+++ flow/experiments/data_loader_1gb.py
@@ -8,22 +8,20 @@ from matplotlib import pyplot as plt
 
 # Dataset to use with the dataloader of the npy files with samples
 class NPYDataset(Dataset):
-    
+
     def __init__(self, filename):
         'Initialisation'
-        samples = np.load(filename)
+        self.samples = np.load(filename)
+        #self._samples_min = self.samples.min(axis=0)[:-1]
+        #self._samples_max = self.samples.max(axis=0)[:-1]
 
-        samples[:,0] = np.log(samples[:,0])
-        samples[:,1] = np.sign(samples[:,1])*np.log10(np.abs(samples[:,1]))
-
-        self._samples_min = samples.min(axis=0)
-        self._samples_max = samples.max(axis=0)
- 
-        self.samples = 2.0*(samples - self._samples_min)/(self._samples_max - self._samples_min) - 1.0
-            
+        self._samples_min = 0.0
+        self._samples_max = 1.0
+           
     def __getitem__(self, index):
         'Generates one sample of data' 
-        return self.samples[index,:]
+        sampl = 2.0*(self.samples[index,:-1] - self._samples_min)/(self._samples_max - self._samples_min) - 1.0
+        return sampl
 
     def __len__(self):
         'Denotes the total number of samples'
@@ -31,7 +29,7 @@ class NPYDataset(Dataset):
 
     def plot_distribution(self):
         figure = corner.corner(self.samples)
-        plt.savefig('samples_f.png')
+        plt.savefig('samples.png')
         plt.close()
 
     @property
@@ -44,4 +42,5 @@ class NPYDataset(Dataset):
 
     @property
     def labels(self):
-        return ['f0', 'fdot']
+        return ['amp','f0', 'fdot', 'beta', 'lam', 'iota', 'psi', 'phi0']
+
