bfc6eef23cac5e20d04745271446c50ba5ed2d66
Natalia Korsakova <natalia.korsakova@obspm.fr>
2023-12-22 18:42:35 +0100

    Undated scripts for testing

--- flow/experiments/rvs/test_galaxy_ffdot.py
+++ flow/experiments/rvs/test_galaxy_ffdot.py
@@ -4,35 +4,37 @@ Test how sampling fr
 import argparse
 import numpy as np
 
-from galaxy import Galaxy
+from galaxy_ffdot import GalaxyFFdot
 #from test import Test
 
 def main(parser):
 
     # Parse command line arguments
-    parser.add_argument('--config', type=str, default='../configs/gbs/density_galaxy.yaml',
+    parser.add_argument('--config', type=str, default='../configs/gbs/density_f.yaml',
                         help='Path to config file specifying model architecture and training procedure')
     args = parser.parse_args()
-    gal = Galaxy(args.config)
-
+    gal = GalaxyFFdot(args.config)
+    # Path with the min and max for the distribution of the galaxy
     gal.load_fit()
 
-    path_minmax = 'minmax_galaxy_sangria.txt'
-    param_min, param_max = np.loadtxt(path_minmax)
+    path_minmax = 'minmax_ffdot_sangria.txt'
+    param_min, param_max = np.loadtxt(path_minmax) 
     gal.set_min(param_min)
     gal.set_max(param_max)
 
-    samples = gal.sample(5000000)
+    samples = gal.sample(1000)
     print('samples = ', samples)
 
     log_prob = gal.log_prob(samples)
     print('log_prob = ', log_prob)
-    
+
     #samples_np = samples.get()
     # Save samples to npy file
     #np.save('samples_galaxy_93_1e5.npy', samples_np)
 
-
 if __name__=='__main__':
-    parser = argparse.ArgumentParser(description = 'Sample galaxy: amplitude and sky localisation')
+    parser = argparse.ArgumentParser(description = 'Sample galaxy f and fdot')
     main(parser)
+
+
+
