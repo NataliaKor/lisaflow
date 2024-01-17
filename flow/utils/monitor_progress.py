772210cd2a7738862a02fd8b6ac02160a9ab5534
Natalia Korsakova <natalia.korsakova@obspm.fr>
2024-01-17 11:44:20 +0100

    Make finctions to plot up to data

--- flow/utils/monitor_progress.py
+++ flow/utils/monitor_progress.py
@@ -6,6 +6,43 @@ import corner
 import matplotlib.pyplot as plt
 from scipy import stats
 from flow.utils.transform_to_as import * 
+import h5py
+
+
+def renormalise_gb_samples(pars, amp_true, freq_min, freq_max):
+    '''
+      Prior ranges for GB densities. Used to renormalise results of sampling back to physical densities.
+      Inputs: 
+          pars -- normalised samples
+          amp_true -- amplitude value for the injected signal
+          freq -- vector of frequencies for injected signal. I might have differet vectors
+                  so maybe I should only take the vector in the range.
+    '''
+    num_params = pars.shape[1]-1
+    print('pars.shape = ', pars.shape)
+    # Prior ranges for the parameters
+    prior = np.zeros((num_params, 2))
+    prior[0,:] = np.log10(np.array([amp_true/100, amp_true*100]))
+    prior[1,:] = np.array([freq_min, freq_max])
+    prior[2,:] = np.array([-1.e-14, 1.e-14])
+    prior[3,:] = np.array([-1.0, 1.0])
+    prior[4,:] = np.array([0.0, 2.0*np.pi])
+    prior[5,:] = np.array([-1.0, 1.0])
+    prior[6,:] = np.array([0.0, 2.0*np.pi])
+    prior[7,:] = np.array([0.0, 2.0*np.pi])
+    
+    print('prior[0,:] = ', prior[0,:])
+
+    # Normalise back to the physical parameters range
+    pp = np.empty_like(pars[:,:-1])
+    for ii in range(num_params):
+        pp[:,ii] = prior[ii, 0] + pars[:,ii]*(prior[ii, 1] - prior[ii, 0])
+    #pp[:,0] = 10.**pp[:,0]
+    #pp[:,3] = np.arcsin(pp[:,3])
+    #pp[:,5] = np.arccos(pp[:,5])
+    # Return samples in the physical range of parameters
+    ind = [1,2,3,4,5,0,7,6]
+    return pp[:,ind]
 
 
 def make_pp(percentiles, parameter_labels, iteration, test_label, ks=True):
@@ -41,33 +78,40 @@ def make_pp(percentiles, parameter_labels, iteration, test_label, ks=True):
     plt.close()
 
 
-def make_cp_compare_samples_gb(flow, iteration, labels, param_mean, param_std, coeff_norm, truths, test_label, filename):
+def make_cp_compare_samples_gb(flow, iteration, labels, param_mean, param_std, coeff_norm, truths, test_label, filename, amp_true, freq_min, freq_max):
 
     # TODO
     # This is the type for the files that Stas has provided me.
-    # But I need to change it for the stype of the files that we get from Eryn.
-    samples_comparison = np.load(filename)
+    # But I need to change it for the type of the files that we get from Eryn.
 
-    num_samples = 200
+    samples_comparison =  renormalise_gb_samples(np.load(filename), amp_true, freq_min, freq_max)
+    num_samples = 10000
 
-    print('coeff_norm.shape = ', coeff_norm.shape)
     samples = flow.sample(num_samples, coeff_norm).squeeze()
-
-    for i in range(0, 100):
-        samples_temp = flow.sample(num_samples, coeff_norm).squeeze()
-        samples = torch.vstack([samples, samples_temp])
-
-    for j in range(param_mean.shape[0]):
-        samples[:,j] = samples[:,j]*param_std[j] + param_mean[j]
-
-    # Plot a_s at the moment but for the future I need to reconstruct the physical parameters
-
-    figure = corner.corner(samples.cpu().detach().numpy(),
+    samples = samples * param_std + param_mean
+    fig = corner.corner(samples_comparison[:,[0,1,2,3]], 
+                        labels=labels,
+                        show_titles = True,
+                        plot_datapoints=False, 
+                        fill_contours=True, 
+                        bins=50,
+                        levels=[0.68, 0.954, 0.997], 
+                        color='red',
+                        plot_density=True)
+ 
+    corner.corner(samples.cpu().detach().numpy(),
+             fig = fig,
              labels=labels,
-             show_titles=True, truths=truths)
+             show_titles=True, 
+             plot_dataponts = False,
+             fill_contours=True,
+             bins=50,
+             levels=[0.68, 0.954, 0.997],
+             color='blue',
+             plot_density=True,
+             truths=truths)
     plt.savefig('../experiments/plots/corner_' + str(test_label) + '_'+str(iteration)+'.png')
     plt.close()
-
 
 
 def make_cp_compare_samples_mbhb(flow, iteration, labels, param_mean, param_std, coeff_norm, truths, test_label):
@@ -197,8 +241,8 @@ def make_cp_density_estimation(flow, iteration, labels, param_min, param_max, te
         samples = np.vstack([samples, samples_temp])
 
     for j in range(param_min.shape[0]):
-        samples[:,j] = param_min[j] + samples[:,j]*(param_max[j] - param_min[j])
-
+        #samples[:,j] = param_min[j] + samples[:,j]*(param_max[j] - param_min[j])
+        samples[:,j] = param_min[j] + (samples[:,j] + 1.0)*(param_max[j] - param_min[j])/2.0
 
     #samples_load = np.load('/sps/lisaf/natalia/github/ai_for_lisa/lisaflow/flow/experiments/samples/run_1_accepted_rj_points.npy')
     #print('samples_load.shape = ', samples_load.shape)
@@ -221,16 +265,17 @@ def make_cp_density_estimation(flow, iteration, labels, param_min, param_max, te
              show_titles=True,
              plot_datapoints=True,
              fill_contours=True, 
-             bins=50,
-             quantiles=[0.68, 0.954, 0.997],
+             bins=100,
+             levels=[0.68, 0.954, 0.997],
              color='blue',
-             weights=np.ones(samples.shape[0])/samples.shape[0],
              plot_density=True)
     plt.savefig('../experiments/plots/corner_' + str(test_label) + '_'+str(iteration)+'.png')
     plt.close()
 
 
 def make_cp_density_estimation_minus1(flow, iteration, labels, param_min, param_max, test_label, filename):
+# TODO remove loops
+# Plot Galaxy for comparison 
   
     num_samples = 200
 
@@ -243,35 +288,112 @@ def make_cp_density_estimation_minus1(flow, iteration, labels, param_min, param_
     for j in range(param_min.shape[0]):
         samples[:,j] = param_min[j] + (samples[:,j] + 1.0)*(param_max[j] - param_min[j])/2.0
 
+    samples_data = np.load(filename)
+    #with h5py.File(filename, "r") as f:
+    #    load_data = f['gbs_sky_dist'][()]
+    #    samples = np.array(load_data.tolist())
+    #    samples[:,0] = np.log10(self.samples[:,0])
+    #fig = corner.corner(samples_data[:,1:-4], 
+    #                       labels = labels,  
+    #                       color = 'red',
+    #                       plot_datapoints=False,
+    #                       fill_contours=True,
+    #                       bins = 50,
+    #                       levels=[0.68,0.954,0.997],
+    #                       weights=np.ones(samples_data.shape[0])/samples_data.shape[0],
+    #                       plot_density=True)
 
-    #samples_load = np.load('/sps/lisaf/natalia/github/ai_for_lisa/lisaflow/flow/experiments/samples/run_1_accepted_rj_points.npy')
-    #print('samples_load.shape = ', samples_load.shape)
-    #fig = corner.corner(samples_load, 
-    #                    labels=labels,
-    #                    show_titles = True,
-    #                    plot_datapoints=False, 
-    #                    fill_contours=True, 
-    #                    bins=50,
-    #                    quantiles=[0.68, 0.954, 0.997], 
-    #                    color='red',
-    #                    plot_density=True)
-    #plt.savefig('samples_original.png')
-    #plt.close()
+    corner.corner(samples, #            fig = fig,
+             labels=labels,
+             show_titles=True,
+             plot_datapoints=False,
+             fill_contours=True, 
+             bins=50,
+             quantiles=[0.68, 0.954, 0.997],
+             color='blue',
+             weights=np.ones(samples.shape[0])/samples.shape[0],
+             plot_density=True)
+    plt.savefig('../experiments/plots/corner_' + str(test_label) + '_'+str(iteration)+'.png')
+    plt.close()
 
 
-    #print('samples.shape = ', samples.shape)
-#    samples_data = np.load(filename)
-#    figure = corner.corner(samples_data[:,:-1], 
-#                           labels = labels,  
-#                           color = 'gray',
-#                           plot_datapoints=False,
-#                           fill_contours=True,
-#                           bins = 50,
-#                           levels=[0.68,0.954,0.997],
-#                           weights=np.ones(samples_data.shape[0])/samples_data.shape[0],
-#                           plot_density=True)
+def make_cp_density_estimation_gb_sampling_points(flow, iteration, labels, param_min, param_max, test_label, filename):
+# TODO remove loops
+# Plot Galaxy for comparison 
+  
+    num_samples = 200
 
-    corner.corner(samples, #fig = figure,
+    samples = flow.sample(num_samples).squeeze().cpu().detach().numpy()
+
+    #for i in range(0, 50):
+    #    samples_temp = flow.sample(num_samples).squeeze().cpu().detach().numpy()
+    #    samples = np.vstack([samples, samples_temp])
+    
+    samples = param_min + (samples + 1.0)*(param_max - param_min)/2.0
+
+    samples_data = np.load(filename)
+    # Normalise parameters to the physical ranges
+    
+    fig = corner.corner(samples_data[:,1:-4], 
+                           labels = labels,  
+                           color = 'red',
+                           plot_datapoints=False,
+                           fill_contours=True,
+                           bins = 50,
+                           levels=[0.68,0.954,0.997],
+                           weights=np.ones(samples_data.shape[0])/samples_data.shape[0],
+                           plot_density=True)
+
+    corner.corner(samples, 
+             fig = fig,
+             labels=labels,
+             show_titles=True,
+             plot_datapoints=False,
+             fill_contours=True, 
+             bins=50,
+             quantiles=[0.68, 0.954, 0.997],
+             color='blue',
+             weights=np.ones(samples.shape[0])/samples.shape[0],
+             plot_density=True)
+    plt.savefig('../experiments/plots/corner_' + str(test_label) + '_'+str(iteration)+'.png')
+    plt.close()
+
+
+
+
+def make_cp_density_estimation_minus1_galaxy(flow, iteration, labels, param_min, param_max, test_label, filename):
+# TODO remove loops
+# Plot Galaxy for comparison 
+  
+    num_samples = 200
+
+    samples = flow.sample(num_samples).squeeze().cpu().detach().numpy()
+
+    for i in range(0, 50):
+        samples_temp = flow.sample(num_samples).squeeze().cpu().detach().numpy()
+        samples = np.vstack([samples, samples_temp])
+
+    for j in range(param_min.shape[0]):
+        samples[:,j] = param_min[j] + (samples[:,j] + 1.0)*(param_max[j] - param_min[j])/2.0
+
+    samples_data = np.load(filename)
+    #with h5py.File(filename, "r") as f:
+    #    load_data = f['gbs_sky_dist'][()]
+    #    samples = np.array(load_data.tolist())
+    #    samples[:,0] = np.log10(self.samples[:,0])
+    #samples_data[:,0] = np.log10(samples_data[:,0])
+    fig = corner.corner(samples_data, 
+                           labels = labels,  
+                           color = 'red',
+                           plot_datapoints=False,
+                           fill_contours=True,
+                           bins = 50,
+                           levels=[0.68,0.954,0.997],
+                           weights=np.ones(samples_data.shape[0])/samples_data.shape[0],
+                           plot_density=True)
+
+    corner.corner(samples, 
+             fig = fig,
              labels=labels,
              show_titles=True,
              plot_datapoints=False,
@@ -311,6 +433,9 @@ def make_cp_density_estimation_01(flow, iteration, labels, test_label):
     plt.close()
 
 
+
+
+
 def waveform_from_posterior():
 
     print('Not implemented')
@@ -323,6 +448,7 @@ def waveform_from_posterior():
 
     # Plot also the results of the embedding to see how the compression works
    
+
 
 
 
