31aed626affe71acd0fa5726e8f18b796fe87669
Natalia Korsakova <natalia.korsakova@obspm.fr>
2025-03-05 14:39:23 +0100

    Adjust init files

--- flow/experiments/__init__.py
+++ flow/experiments/__init__.py
@@ -1,11 +1,1 @@
-from flow.experiments.data_loader import(
-    NPYDataset
-)
 
-from flow.experiments.data_loader_sky import (
-    NPYDataset
-)
-
-from flow.experiments.data_loader_galaxy import (
-    GalaxyDataset
-)
